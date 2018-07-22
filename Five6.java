96372a8e4144323f2cd26d5cade8e4a284765d1b
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-22 14:27:33 +0530

    Print the next Number

---
+++ Five6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Five6 {
+	public static void main(String[] args) {
+		int a;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		if(a<=100000){
+		System.out.println(a+1);
+	}
+	}
+}
