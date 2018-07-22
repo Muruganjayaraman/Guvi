38fa5b256b865cb7ef660a07798ffb06ba102fce
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-22 11:05:33 +0530

    String length print

---
+++ Seven1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Seven1 {
+	public static void main(String args[]){
+	String a;
+	Scanner in=new Scanner(System.in);
+	a=in.next();
+	int b=in.nextInt();
+	a=a.substring(0,b);
+	System.out.println(a);
+
+}
+}
