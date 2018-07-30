3cf18ecc6ed8daf569dd1bc197d64ae27fa291c5
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-30 10:25:12 +0530

    Word Count and length

--- WordCount.java
+++ WordCount.java
@@ -2,15 +2,17 @@ package beginner4;
 import java.util.Scanner;
 public class WordCount {
 	public static void main(String args[]){
-		String a;
+		String s;
 		int count=0;
 		Scanner in=new Scanner(System.in);
-		a=in.nextLine();
-		String b[]=a.split("[.]+");
+		s=in.nextLine();
+		if(s.length()<=1000){
+		String b[]=s.split("[.]+");
 		int c=b.length;
 		System.out.println(c);
 		in.close();
 	}
+}
 }
 
 
