10c6e57520c9b6bfb968ea788ddf5f0743f04b6c
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-31 09:12:53 +0530

    Sum of Two Numbers

---
+++ SumNum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SumNum {
+	public static void main(String args[]){
+		int a,b,sum=0;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		sum=a+b;
+		System.out.println(sum);
+		in.close();
+	}
+
+}
